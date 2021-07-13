dffa74fd15674c96d53bd7952baa41c6ebefe392
陳鈞 <jim60105@gmail.com>
2021-07-13 23:40:18 +0800

    修正ssh-keyscan

--- volume-backup/script/upload.sh
+++ volume-backup/script/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Starting Transfer to backup server..."
+URL=$(echo "${BACKUP_RSYNC_URI}" | sed -r 's/.*@(.*)::.*/\1/')
+
 su - root -c "mkdir -p ~/.ssh"
-su - root -c "ssh-keyscan -p ${BACKUP_RSYNC_PORT} ${BACKUP_RSYNC_URI} >> ~/.ssh/known_hosts"
+su - root -c "ssh-keyscan -p ${BACKUP_RSYNC_PORT} ${URL} >> ~/.ssh/known_hosts"
 su - root -c "sshpass -f /run/secrets/rsyncpass rsync -e 'ssh -p ${BACKUP_RSYNC_PORT}' -avz --no-p --no-g /backup/ ${BACKUP_RSYNC_URI}"
